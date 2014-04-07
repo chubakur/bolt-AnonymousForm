f5ee1a0b750206e684817e26f68b6977fe1b3a5d
Andrey Pitko <chubakur@gmail.com>
2014-04-07 12:04:15 +0400

    Twig function for form action generation

--- AnonymouseForm/extension.php
+++ AnonymouseForm/extension.php
@@ -43,17 +43,27 @@ class Extension extends \Bolt\BaseExtension
     public function initialize()
     {
         $this->config = $this->getConfig();
-        $this->app->match($this->config['prefix'].'{contenttype}', array($this, 'formHandler'));
+        $this->app->match($this->config['prefix'] . '{contenttype}', array($this, 'formHandler'));
+        $this->addTwigFunction('anonymouseFormPath', 'anonymouseFormPath');
     }
 
-    public function formHandler($contenttype, Request $request){
-        if ( $request->isMethod('POST') && in_array($contenttype, $this->config['contenttypes'])){
+    public function anonymouseFormPath($contenttype)
+    {
+        if ( !in_array($contenttype, $this->config['contenttypes']) )
+            throw new \Exception("Wrong contenttype");
+        return $this->config['prefix'] . $contenttype;
+    }
+
+    public function formHandler($contenttype, Request $request)
+    {
+        if ($request->isMethod('POST') && in_array($contenttype, $this->config['contenttypes'])) {
             $storage = $this->app['storage'];
             $content = $storage->getContentObject($contenttype);
-            foreach($request->request->all() as $key=>$value){
+            foreach ($request->request->all() as $key => $value) {
                 $content->setValue($key, $value);
             }
             $storage->saveContent($content);
+
             return $this->app->redirect($request->headers->get('referer'));
         }
     }
