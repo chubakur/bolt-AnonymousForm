dec7a065800da12b39257604436e4c50b5eb17a0
Andrey Pitko <chubakur@gmail.com>
2014-04-16 15:30:42 +0400

    recaptcha

--- AnonymousForm/extension.php
+++ AnonymousForm/extension.php
@@ -30,12 +30,12 @@ class Extension extends \Bolt\BaseExtension
             'type' => "General",
             'author' => "Andrey Pitko",
             'link' => "http://artvisio.com",
-            'email' => 'chubakur@gmail.com',
-            'version' => "0.1",
+            'email' => 'andrey.p@artvisio.com',
+            'version' => "0.2",
             'required_bolt_version' => "1.4",
             'highest_bolt_version' => "1.5.5",
             'first_releasedate' => "2014-04-07",
-            'latest_releasedate' => "2014-04-07"
+            'latest_releasedate' => "2014-04-16"
         );
 
     }
@@ -45,6 +45,12 @@ class Extension extends \Bolt\BaseExtension
         $this->config = $this->getConfig();
         $this->app->match($this->config['prefix'] . '{contenttype}', array($this, 'formHandler'));
         $this->addTwigFunction('anonymouseFormPath', 'anonymouseFormPath');
+        $this->addTwigFunction('getCaptchaFormField', 'getCaptchaFormField');
+    }
+
+    public function getCaptchaFormField(){
+        require_once('recaptcha/recaptchalib.php');
+        return recaptcha_get_html($this->config['captcha']['public']);
     }
 
     public function anonymouseFormPath($contenttype)
@@ -57,6 +63,20 @@ class Extension extends \Bolt\BaseExtension
     public function formHandler($contenttype, Request $request)
     {
         if ($request->isMethod('POST') && in_array($contenttype, $this->config['contenttypes'])) {
+            if ( isset($this->config['captcha']['enabled'])
+                && is_array($this->config['captcha']['enabled'])
+                && in_array($contenttype, $this->config['captcha']['enabled'])
+            ){
+                require_once('recaptcha/recaptchalib.php');
+                $resp = recaptcha_check_answer ($this->config['captcha']['private'],
+                    $_SERVER["REMOTE_ADDR"],
+                    $request->request->get('recaptcha_challenge_field'),
+                    $request->request->get('recaptcha_response_field'));
+                if (!$resp->is_valid) {
+                    die ("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
+                        "(reCAPTCHA said: " . $resp->error . ")");
+                }
+            }
             $storage = $this->app['storage'];
             $content = $storage->getContentObject($contenttype);
             foreach ($request->request->all() as $key => $value) {
